b9360e83a12b87a86cef16f59eae782fb7705285
jhlim <jhlim@crscube.io>
2021-03-11 14:09:49 +0900

    테라폼 기본 코드

---
+++ main.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+	region = var.aws_region
+    shared_credentials_file = "/Users/jonghyunlim/.aws/credentials"
+    profile = "dlsrks1218"
+}
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "ap-northeast-2"
+}
+
+
+
